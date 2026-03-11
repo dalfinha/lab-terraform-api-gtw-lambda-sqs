16a32959a88382f9a6c0e4fe49ce148e829a1a69
Dalva <dalva.mariana.dm@gmail.com>
2026-03-11 19:54:46 -0300

    fix: function name, locals tag

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -1,11 +1,11 @@
 module "lambda" {
   source  = "terraform-aws-modules/lambda/aws"
 
-  function_name = "processor-${var.project_name}"
-  description   = "Lambda para processamento de compras recebidas pelo API Gateway ${var.api_gateway_name}"
+  function_name = "lambda-${var.project_name}"
+  description   = "Lambda para processamento de compras recebidas pelo API Gateway ${var.api_gateway_name} e processamento com a fila ${module.sqs_queue.queue_name}"
 
   handler = "lambda_function.handler"
-  runtime = "python3.14"
+  runtime = "python3.13"
 
   source_path = "../code"
 
@@ -13,9 +13,5 @@ module "lambda" {
     QUEUE_URL = module.sqs_queue.queue_url
   }
 
-  tags = {
-    environment    = var.environment
-    repository     = "lab-terraform-api-gtw-lambda-sqs"
-    repository_id  = var.github_repository_id
-  }
+  tags = local.all_tags
 }
